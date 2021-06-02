4c57af6d5176a3a8d71eb719710693ac7e292bae
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2021-06-02 11:32:18 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//askdjs;kfj;sdlkfj;aldskfj;lkasdjf;lksdaj;flkjsa;dlkfja;sdklfj;asdkfl
+//askdjs;kfj;sdlkfj;aldskfj;lkasdjf;lksdaj;flkjsa;dlkfja;sdklfj;asdkfld
 int main(){
   printf("<h1>Hello, hongik!</h1>\n");
   return 0;
